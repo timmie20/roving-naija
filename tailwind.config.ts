464e4480c7ad23e1880f2e1d061003289499f45c
olutimilehinolayinka1234@gmail.com <olutimilehinolayinka1234@gmail.com>
2024-11-20 12:10:21 +0100

    First commit, add global styling assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,32 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "app-white": "var(--app-white)",
+        "app-dark": "var(--app-dark)",
+
+        primary: {
+          snow: "var(--primary-snow)",
+          light: "var(--primary-light)",
+          normal: "var(--primary-normal)",
+          dark: "var(--primary-dark)",
+          darker: "var(--primary-darker)",
+        },
+
+        secondary: {
+          snow: "var(--secondary-snow)",
+          light: "var(--secondary-light)",
+          normal: "var(--secondary-normal)",
+          dark: "var(--secondary-dark)",
+          darker: "var(--secondary-darker)",
+        },
+
+        neutral: {
+          "50": "var(--neutral-50)",
+          "100": "var(--neutral-100)",
+          "200": "var(--neutral-200)",
+          "300": "var(--neutral-300)",
+          "400": "var(--neutral-400)",
+        },
       },
     },
   },
